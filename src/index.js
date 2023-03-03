9494ab30ce38306cd1afbbfc4490ff7cbb706077
Travis Roy <travisroydev@gmail.com>
2023-03-02 20:07:46 -0500

    Added support for sourceIP, tcpFlags, and conntrack
    - Removed double quotions for comments in rule object

--- src/index.js
+++ src/index.js
@@ -14,21 +14,24 @@ function parseRule(rule) {
   const [, protocol] = tryMatch(/\s-p\s([A-Za-z]+)/, rule);
   const [, source] = tryMatch(/\s-s\s([^\s]+)/, rule);
   const [, sourcePort] = tryMatch(/\s--sport\s([^\s]+)/, rule);
+  const [, sourceIp] = tryMatch(/\s--to-source\s([^\s]+)/, rule);
   const [, destination] = tryMatch(/\s-d\s([^\s]+)/, rule);
   const [, destinationPort] = tryMatch(/\s--dport\s([^\s]+)/, rule);
   const [, destinationIp] = tryMatch(/\s--to-destination\s([^\s]+)/, rule);
-  const [,match] = tryMatch(/\s-m\s((?!state|comment|limit)[^\s]+)\s/, rule);
+  const [, tcpFlags] = tryMatch(/\s--tcp-flags\s([^\s]+\s(SYN)?)/, rule);
+  const [, match] = tryMatch(/\s-m\s((?!state|comment|limit|conntrack)[^\s]+)\s/, rule);
   const [, jump] = tryMatch(/\s-j\s([^\s]+)/, rule);
   const [, goto] = tryMatch(/\s-g\s([^\s]+)/, rule);
   const [, inInterface] = tryMatch(/\s-i\s([^\s]+)/, rule);
   const [, outInterface] = tryMatch(/\s-o\s([^\s]+)/, rule);
 
   const [, state] = tryMatch(/\s-m\sstate\s--state\s([^\s]+)/, rule);
+  const [, conntrack] = tryMatch(/\s-m\sconntrack\s--ctstate\s([^\s]+)/, rule);
 
   const [, limit] = tryMatch(/\s-m limit --limit\s([^\s]+)/, rule);
   const [, logPrefix] = tryMatch(/\s--log-prefix\s("[^"]+")/, rule);
   const [, tos] = tryMatch(/\s--set-tos\s([^\s]+)/, rule);
-  const [, comment] = tryMatch(/\s-m\scomment\s--comment\s("[^"]+")/, rule);
+  const [, comment] = tryMatch(/\s-m\scomment\s--comment\s"(.+)"/, rule);
 
   return { 
     rule,
@@ -36,11 +39,14 @@ function parseRule(rule) {
     protocol,
     source,
     sourcePort,
+    sourceIp,
     destination,
     destinationPort,
     destinationIp,
+    tcpFlags,
     match,
     state,
+    conntrack,
     jump,
     goto,
     inInterface,
@@ -86,11 +92,14 @@ function encodeRule({
   protocol,
   source,
   sourcePort,
+  sourceIp,
   destination,
   destinationPort,
   destinationIp,
+  tcpFlags,
   match,
   state,
+  conntrack,
   inInterface,
   outInterface,
   limit,
@@ -109,21 +118,24 @@ function encodeRule({
 
   return map2Str('-A', chain) +
     map2Str('-p', protocol) +
-    map2Str('-s', source) +
-    map2Str('--sport', sourcePort) +
-    map2Str('-d', destination) +
-    map2Str('--dport', destinationPort) +
-    map2Str('-m', match) +
-    map2Str('-m state --state', state) +
     map2Str('-i', inInterface) +
     map2Str('-o', outInterface) +
+    map2Str('-s', source) +
+    map2Str('-d', destination) +
+    map2Str('-m', match) +
+    map2Str('--sport', sourcePort) +
+    map2Str('--dport', destinationPort) +
+    map2Str('--tcp-flags', tcpFlags) +
+    map2Str('-m state --state', state) +
+    map2Str('-m conntrack --ctstate', conntrack) +
     map2Str('-m limit --limit', limit) +
     map2Str('-j', jump) +
     map2Str('-g', goto) +
     map2Str('--to-destination', destinationIp) +
+    map2Str('--to-source', sourceIp) +
     map2Str('--log-prefix', logPrefix) +
     map2Str('--set-tos', tos) +
-    map2Str('-m comment --comment', comment)
+    map2Str('-m comment --comment', comment ? `"${comment}"` : null)
       .trim();
 }
 
